02ed12f8935bef1385eabb8b8483dbfa086b3959
zezzz <zezzz@zezs-mac-mini.lan>
2024-05-14 01:28:56 +0900

    test rocm.

--- scripts/vit_imagenet.sh
+++ scripts/vit_imagenet.sh
@@ -1,3 +1,24 @@
+#!/bin/bash
+#SBATCH -A bif146
+#SBATCH -o vit.o%J
+#SBATCH -t 08:00:00
+#SBATCH -N 1
+#SBATCH -p batch
+
+export MIOPEN_DISABLE_CACHE=1 
+export MIOPEN_CUSTOM_CACHE_DIR='pwd' 
+export HOME="/tmp/srun"
+
+# export PATH="/lustre/orion/bif146/world-shared/gvit/dataset/miniconda_frontier/bin:$PATH"
+
+# set +x
+# source /lustre/orion/bif146/world-shared/gvit/dataset/miniconda_frontier/etc/profile.d/conda.sh
+# conda activate /lustre/orion/bif146/world-shared/gvit/dataset/miniconda_frontier/envs/gvit
+
+module load PrgEnv-gnu
+module load gcc/12.2.0
+module load rocm/5.7.0
+
 python main.py \
     --data_dir /lustre/orion/bif146/world-shared/enzhi/imagenet2012 \
     --batch_size 32 \
