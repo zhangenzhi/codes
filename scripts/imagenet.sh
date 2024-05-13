80b4e49d74c8a4868bcd459eeafff54c2f388082
zezzz <zezzz@zezs-mac-mini.lan>
2024-05-13 16:59:05 +0900

    test rocm.

--- scripts/imagenet.sh
+++ scripts/imagenet.sh
@@ -1,4 +1,4 @@
 python main.py \
-    --data_dir /lustre/orion/bif146/world-shared/enzhi/imagenet \
+    --data_dir /lustre/orion/bif146/world-shared/enzhi/imagenet2012 \
     --batch_size 32 \
     --num_workers 64
