b41218fab2da7fcb694ccf1f58cb2fcccb691851
zezzz <zezzz@zezs-mac-mini.lan>
2024-05-15 17:00:29 +0900

    train vit on imagenet.

--- train/vit_ddp.py
+++ train/vit_ddp.py
@@ -82,7 +82,7 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, num_epoch
 
             # Print training progress (optional)
             running_loss += loss.item()
-            if i % 100 == 99:  # Print every 100 mini-batches
+            if i % 100 == 99 and device_id==0:  # Print every 100 mini-batches
                 print('[%d, %5d] loss: %.3f' %
                       (epoch + 1, i + 1, running_loss / 100))
                 running_loss = 0.0
