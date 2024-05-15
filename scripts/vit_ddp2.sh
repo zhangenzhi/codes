0d653395b884f4753688ed0a9d7ef115c3038288
zezzz <zezzz@zezs-mac-mini.lan>
2024-05-15 19:17:30 +0900

    train vit on imagenet.

---
+++ scripts/vit_ddp2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH -A bif146
+#SBATCH -o vit-ddp2.o%J
+#SBATCH -t 02:00:00
+#SBATCH -N 2
+#SBATCH -p batch
+
+export MIOPEN_DISABLE_CACHE=1 
+export MIOPEN_CUSTOM_CACHE_DIR='pwd' 
+export HOME="/tmp/srun"
+
+# export PATH="/lustre/orion/bif146/world-shared/gvit/dataset/miniconda_frontier/bin:$PATH"
+
+# set +x
+# source /lustre/orion/bif146/world-shared/gvit/dataset/miniconda_frontier/etc/profile.d/conda.sh
+# conda activate /lustre/orion/bif146/world-shared/gvit/dataset/miniconda_frontier/envs/gvit
+
+module load PrgEnv-gnu
+module load gcc/12.2.0
+module load rocm/5.7.0
+
+srun -n 2 -c 16 python main.py \
+    --task vit_ddp \
+    --data_dir /lustre/orion/bif146/world-shared/enzhi/imagenet2012 \
+    --batch_size 32 \
+    --num_workers 16 \
+    --num_epochs 30
