0809af05fd8add241b9c7e2310ba7dbe40a1b1c1
zezzz <zezzz@zezs-mac-mini.lan>
2024-05-18 02:00:40 +0900

    logging info.

--- scripts/vit_ddp2.sh
+++ scripts/vit_ddp2.sh
@@ -2,7 +2,7 @@
 #SBATCH -A bif146
 #SBATCH -o vit-ddp2.o%J
 #SBATCH -t 02:00:00
-#SBATCH -N 4
+#SBATCH -N 8
 #SBATCH -p batch
 
 export MIOPEN_DISABLE_CACHE=1 
@@ -21,11 +21,11 @@ module load PrgEnv-gnu
 module load gcc/12.2.0
 module load rocm/5.7.0
 
-srun -N 4 -n 32 --ntasks-per-node 8 python main.py \
+srun -N 8 -n 64 --ntasks-per-node 8 python main.py \
     --task vit_ddp \
-    --logname train-4.log\
+    --logname train-8.log\
     --gpus 8\
-    --nodes 4\
+    --nodes 8\
     --data_dir /lustre/orion/bif146/world-shared/enzhi/imagenet2012 \
     --batch_size 32 \
     --num_workers 8 \
