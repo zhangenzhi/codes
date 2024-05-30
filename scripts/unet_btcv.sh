7ad06963569a91d3bb258ecba373436293358017
zhangenzhi <zhangenzhi657@gmail.com>
2024-05-30 14:26:36 +0900

    btcv dataloader.

--- scripts/unet_btcv.sh
+++ scripts/unet_btcv.sh
@@ -20,9 +20,9 @@ module load gcc/12.2.0
 module load rocm/5.7.0
 
 python main.py \
-    --task unet_btcv \
+    --task unet3d_btcv \
     --pretrained True \
-    --data_dir /lustre/orion/bif146/world-shared/enzhi/imagenet2012 \
+    --data_dir /lustre/orion/bif146/world-shared/enzhi/btcv/data \
     --batch_size 1 \
     --num_workers 4 \
     --num_epochs 30
