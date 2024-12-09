ebcf63937e20881e8f16f03062c4540014ef8968
zezzz <zezzz@zezs-mac-mini.lan>
2024-12-09 16:06:42 +0900

    imagenet ap.

--- dataset/imagenet_ap.py
+++ dataset/imagenet_ap.py
@@ -113,5 +113,5 @@ if __name__ == "__main__":
     for phase in ['train', 'val']:
         for step, data in enumerate(train_loader):
             if step%500==0:
-                print("Step:{} Time cost for loading {}".format(step, time.time() - start_time))
+                print("Step:{} Time cost for loading {}".format(step, time.time() - start_time)//step)
     print("Time cost for loading {}".format(time.time() - start_time))
