ac328e91ae5b36b8524370a2d35e9a41d8a49f2e
zezzz <zezzz@zezs-mac-mini.lan>
2024-12-09 14:02:23 +0900

    imagenet ap.

---
+++ scripts/imagenet_ap.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH -A bif146
+#SBATCH -o vit_imagenet.o%J
+#SBATCH -t 02:00:00
+#SBATCH -N 1
+#SBATCH -p batch
+
+# export MIOPEN_DISABLE_CACHE=1 
+# export MIOPEN_CUSTOM_CACHE_DIR='pwd' 
+# export HOME="/tmp/srun"
+
+# # export PATH="/lustre/orion/bif146/world-shared/gvit/dataset/miniconda_frontier/bin:$PATH"
+
+# # set +x
+# # source /lustre/orion/bif146/world-shared/gvit/dataset/miniconda_frontier/etc/profile.d/conda.sh
+# # conda activate /lustre/orion/bif146/world-shared/gvit/dataset/miniconda_frontier/envs/gvit
+
+# module load PrgEnv-gnu
+# module load gcc/12.2.0
+# module load rocm/5.7.0
+
+python ./dataset/imagenet_ap.py \
+    --task vit_imagenet \
+    --data_dir ../dataset/imagenet2012 \
+    --batch_size 256 \
+    --num_workers 32 \
+    --num_epochs 10
