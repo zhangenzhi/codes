6bc4039060c0070a3474b8f28c027b912341366c
zezzz <zezzz@zezs-mac-mini.lan>
2024-12-09 23:10:08 +0900

    bert train.

--- train/bert_imagenet_ap.py
+++ train/bert_imagenet_ap.py
@@ -140,7 +140,7 @@ def bert_train(args):
     
     # Define loss function and optimizer
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(),lr=1e-4)
+    optimizer = torch.optim.AdamW(model.parameters(),lr=1e-4)
 
     # Train the model
     train_model(model, train_loader, val_loader, criterion, optimizer, args.num_epochs, args.output)
