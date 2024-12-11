6ec1bd60c24867f6ccff72f86cd0f2098d2d23fb
zezzz <zezzz@zezs-mac-mini.lan>
2024-12-11 23:16:14 +0900

    test normalize and opt lr 1e-3

--- dataset/imagenet_ap.py
+++ dataset/imagenet_ap.py
@@ -26,7 +26,7 @@ class ImageNetDataset(Dataset):
         self.transform =  transforms.Compose([
             # transforms.RandomResizedCrop(224),
             # transforms.RandomHorizontalFlip(),
-            transforms.Resize([224,224])
+            transforms.Resize([224,224]),
             transforms.ToTensor(),
             # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
