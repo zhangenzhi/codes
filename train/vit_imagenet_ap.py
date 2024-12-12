5e3500293348068f8ad3affbf2d5a4a0aa13f076
zhangenzhi <zhangenzhi657@gmail.com>
2024-12-12 11:08:25 +0900

    test imagenet loader.

--- train/vit_imagenet_ap.py
+++ train/vit_imagenet_ap.py
@@ -171,7 +171,7 @@ def vit_ap_train(args):
     
     # Define loss function and optimizer
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(),lr=1e-3)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
     # Train the model
     train_model(model, train_loader, val_loader, criterion, optimizer, args.num_epochs)
