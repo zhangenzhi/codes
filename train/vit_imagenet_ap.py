1315eb197dc64a76428f8d1d1b196f5b83e84f83
zhangenzhi <zhangenzhi657@gmail.com>
2024-12-12 11:57:31 +0900

    test imagenet loader.

--- train/vit_imagenet_ap.py
+++ train/vit_imagenet_ap.py
@@ -8,41 +8,9 @@ import os
 from torch.utils.data import DataLoader
 import time
 
-from model.vit import VisionTransformer
+from model.vit import VisionTransformer, create_vit_model
 from dataset.imagenet_ap import ImageNetDataset
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-def create_vit_model(pretrained, num_classes=1000):
-    """
-    Creates a ViT model for ImageNet classification.
-
-    Args:
-        pretrained (bool): If True, loads pre-trained weights. Defaults to False.
-        num_classes (int, optional): Number of output classes (defaults to 1000 for ImageNet). Defaults to 1000.
-
-    Returns:
-        nn.Module: The created ViT model.
-    """
-
-    if pretrained:
-        # Fine-tune a pre-trained model (freeze early layers if desired)
-        model = timm.create_model("vit_base_patch16_224", pretrained=True)
-        for param in model.parameters():
-            param.requires_grad = False  # Optionally freeze early layers
-
-        # Modify the final classification head
-        in_features = model.head.in_features
-        model.head = nn.Linear(in_features, num_classes)
-
-    else:
-        # Create a ViT model with randomly initialized weights
-        model = timm.create_model("vit_base_patch16_224", pretrained=False)
-        # Modify the final classification head
-        in_features = model.head.in_features
-        model.head = nn.Linear(in_features, num_classes)
-        
-    model = nn.DataParallel(model)
-    return model.to(device)
 
 
 def train_model(model, train_loader, val_loader, criterion, optimizer, num_epochs):
@@ -64,7 +32,6 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, num_epoch
     scaler = torch.cuda.amp.GradScaler()
 
     model.train()  # Set model to training mode
-    total_step = len(train_loader)
     best_val_acc = 0.0
     print("Training the ViT model for {} epochs...".format(num_epochs))
 
@@ -101,7 +68,27 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, num_epoch
 
         # Validate after each epoch
         model.eval()
-        val_acc,val_loss = evaluate_model(model, val_loader, criterion)
+        val_correct = 0
+        total = 0
+        val_loss = 0.0
+        num_iter = 0
+        with torch.no_grad():
+            for images, labels in val_loader:
+                images = images.to(device, non_blocking=True)
+                images = torch.reshape(images,shape=(-1,3,224, 224))
+                labels = labels.to(device, non_blocking=True)
+                
+                with torch.cuda.amp.autocast():
+                    outputs = model(images)
+                    loss = criterion(outputs, labels)
+                    
+                num_iter += 1
+                val_loss += loss.item()
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+        val_loss /= num_iter
+        val_acc = 100 * val_correct / total
         print("Val_Acc: {:.4f},Val_Loss: {:.4f}, Time Cost:{}".format(val_acc, val_loss, time.time()-start_time))
 
         # Save the best model based on validation accuracy
@@ -113,41 +100,9 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, num_epoch
 
     print('Finished Training. Best Validation Accuracy: {:.4f}'.format(best_val_acc))
 
-def evaluate_model(model, val_loader, criterion):
-    """
-    Evaluates the model on the validation set.
-
-    Args:
-        model (nn.Module): The trained model.
-        val_loader (DataLoader): The
-            # Put model in evaluation mode
-    """
-    correct = 0
-    total = 0
-    val_loss = 0.0
-    num_iter = 0
-    with torch.no_grad():
-        for images, labels in val_loader:
-            images = images.to(device, non_blocking=True)
-            images = torch.reshape(images,shape=(-1,3,224, 224))
-            labels = labels.to(device, non_blocking=True)
-            with torch.cuda.amp.autocast():
-                outputs = model(images)
-                loss = criterion(outputs, labels)
-                
-            num_iter += 1
-            val_loss += loss.item()
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-    val_loss /= num_iter
-    accuracy = 100 * correct / total
-    return accuracy, val_loss
-
 def vit_ap_train(args):
-
+    
     # Create DataLoader for training and validation
-        # Create DataLoader for training and validation
     train_dir = os.path.join(args.data_dir, "train")
     val_dir = os.path.join(args.data_dir,"val")
 
@@ -171,7 +126,7 @@ def vit_ap_train(args):
     
     # Define loss function and optimizer
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
     # Train the model
     train_model(model, train_loader, val_loader, criterion, optimizer, args.num_epochs)
