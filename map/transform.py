e8ac687b073b2aa92c909f88bfc86caf1077ed11
zhangenzhi <zhangenzhi657@gmail.com>
2024-12-13 17:14:07 +0900

    test origin+ap.

--- map/transform.py
+++ map/transform.py
@@ -21,7 +21,7 @@ class Patchify(torch.nn.Module):
         # self.canny = [c, c+50]
         self.smooth_factor = 0
         if self.smooth_factor ==0 :
-            edges = np.random.uniform(low=0,high=1,size=img.shape)
+            edges = np.random.uniform(low=0,high=1, size=(256,256))
         else:
             grey_img = cv.GaussianBlur(img, (self.smooth_factor, self.smooth_factor), 0)
             edges = cv.Canny(grey_img, self.canny[0], self.canny[1])
