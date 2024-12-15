2ddb17b04739fecbad6c3bebfec9aee73e44ca2b
zezzz <zezzz@zezs-mac-mini.lan>
2024-12-15 21:16:22 +0900

    test seq 514.

--- map/transform.py
+++ map/transform.py
@@ -26,8 +26,7 @@ class Patchify(torch.nn.Module):
         else:
             grey_img = cv.GaussianBlur(img, (self.smooth_factor, self.smooth_factor), 0)
             edges = cv.Canny(grey_img, self.canny[0], self.canny[1])
-        # import pdb
-        # pdb.set_trace()
+
         qdt = FixedQuadTree(domain=edges, fixed_length=self.fixed_length)
         seq_img, seq_size, seq_pos = qdt.serialize(img, size=(self.patch_size,self.patch_size,3))
         seq_size = np.asarray(seq_size)
