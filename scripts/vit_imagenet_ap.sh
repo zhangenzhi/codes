cafb60e8024b03606da9bbb59fcede89d52c6e53
zezzz <zezzz@zezs-mac-mini.lan>
2024-12-17 15:22:26 +0900

    imagenet ap sth 9.

--- scripts/vit_imagenet_ap.sh
+++ scripts/vit_imagenet_ap.sh
@@ -22,6 +22,6 @@
 python main.py \
     --task vit_imagenet_ap \
     --data_dir ../dataset/imagenet2012 \
-    --batch_size 144 \
+    --batch_size 512 \
     --num_workers 32 \
     --num_epochs 100
