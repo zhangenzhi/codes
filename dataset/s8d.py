9e61c0835691d0cac3f059baaa3ebccfb318b78c
zhangenzhi <zhangenzhi657@gmail.com>
2025-01-18 20:24:49 +0800

    s8d datalaoder test.

---
+++ dataset/s8d.py
@@ -0,0 +1,71 @@
+import os
+import sys
+sys.path.append("./")
+import argparse
+from pathlib import Path
+import numpy as np
+from PIL import Image, ImageFile
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+
+# Set the flag to load truncated images
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+class Spring8Dataset(Dataset):
+    def __init__(self, data_path):
+        self.data_path = data_path
+        self.subslides = os.listdir(data_path)
+        self.image_filenames = []
+
+        for subdir in self.subslides:
+            subdir_path = os.path.join(data_path, subdir)
+            if os.path.isdir(subdir_path):
+                sample_path = os.listdir(subdir_path)
+                for sampledir in sample_path:
+                    sample_slice_path = os.path.join(subdir_path, sampledir)
+                    if os.path.isdir(sample_slice_path):
+                        num_sample_slice = len(os.listdir(sample_slice_path))
+                        for i in range(num_sample_slice):
+                            # Ensure the image exist
+                            img_name = f"volume_{i}.raw"
+                            image = os.path.join(sample_slice_path, img_name)
+                            if os.path.exists(image):
+                                self.image_filenames.extend([image])
+
+        print("img tiles: ", len(self.image_filenames))
+        
+        self.transform= transforms.Compose([
+                transforms.ToTensor(),
+            ])
+
+    def __len__(self):
+        return len(self.image_filenames)
+
+    def __getitem__(self, idx):
+        img_name = self.image_filenames[idx]
+        image = Image.open(img_name).convert("L")
+        return image
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', default="miccai", 
+                        help='base path of dataset.')
+    parser.add_argument('--data_dir', default="/lustre/orion/nro108/world-shared/enzhi/spring8data/demo", 
+                        help='base path of dataset.')
+    parser.add_argument('--epoch', default=1, type=int,
+                        help='Epoch of training.')
+    parser.add_argument('--batch_size', default=128, type=int,
+                        help='Batch_size for training')
+    args = parser.parse_args()
+
+    # Example usage
+    dataset = Spring8Dataset(args.data_dir, args.resolution, normalize=False)
+    dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
+
+    # Now you can iterate over the dataloader to get batches of images and masks
+    for batch in dataloader:
+        images, masks = batch
+        print(images.shape, masks.shape)
---
+++ scripts/mae_s8d.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -A nro108
+#SBATCH -o mae_imagenet.o%J
+#SBATCH -t 02:00:00
+#SBATCH -N 1
+#SBATCH -p batch
+
+export MIOPEN_DISABLE_CACHE=1 
+export MIOPEN_CUSTOM_CACHE_DIR='pwd' 
+export HOME="/tmp/srun"
+
+module load PrgEnv-gnu
+module load gcc-native/12.3
+module load rocm/6.2.0
+
+python main.py \
+    --task mae_imagenet \
+    --data_dir /lustre/orion/nro108/world-shared/enzhi/spring8data/ \
+    --batch_size 512 \
+    --num_workers 32 \
+    --num_epochs 100
