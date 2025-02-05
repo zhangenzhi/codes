25171f88b3d0d5f958056bc5e4b08ac938710936
zhangenzhi <zhangenzhi657@gmail.com>
2025-02-05 05:17:37 -0800

    test 2d pos.

--- map/transform.py
+++ map/transform.py
@@ -33,4 +33,6 @@ class ImagePatchify(torch.nn.Module):
         seq_img = np.asarray(seq_img)
         seq_img = np.reshape(seq_img, [self.patch_size*self.patch_size, -1, self.num_channels])
 
+        import pdb
+        pdb.set_trace()
         return seq_img, seq_size, seq_pos
