825d6306d9581cafd4713e168d198f0e08e0b1e9
zhangenzhi <zhangenzhi657@gmail.com>
2025-02-05 05:10:02 -0800

    test 2d pos.

--- train/vit_imagenet_ap.py
+++ train/vit_imagenet_ap.py
@@ -53,6 +53,8 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, num_epoch
         running_loss = 0.0
         correct = 0
         for i, (image, seq_img, seq_size, seq_pos, labels) in enumerate(train_loader):
+            import pdb
+            pdb.set_trace()
             seq_img = seq_img.to(device, non_blocking=True)
             seq_img = seq_img.view(-1, 196, 16*16*3) 
             # images = torch.reshape(images,shape=(-1,3,224, 224))
