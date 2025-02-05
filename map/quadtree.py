5e3e0b4496cc810680577bbbfdb5e4292abbdcbc
zhangenzhi <zhangenzhi657@gmail.com>
2025-02-05 05:34:16 -0800

    test 2d pos.

--- map/quadtree.py
+++ map/quadtree.py
@@ -166,6 +166,7 @@ class FixedQuadTree:
             # random_drop
         assert len(seq_patch)==self.fixed_length, "Not equal fixed legnth."
         assert len(seq_size)==self.fixed_length, "Not equal fixed legnth."
+        print("seq_pos length:",len(seq_pos))
         assert len(seq_pos)==self.fixed_length, "Not equal fixed legnth."
         return seq_patch, seq_size, seq_pos
     
