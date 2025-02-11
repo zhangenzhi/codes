7389e1ebd62b5a8e4c1408a81f62955e4586fdca
zezzz <zezzz@zezs-mac-mini.lan>
2025-02-11 10:32:04 +0900

    test adaptive forest.

--- train/vit_imagenet_af.py
+++ train/vit_imagenet_af.py
@@ -58,6 +58,7 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, num_epoch
             seq_img = seq_img.to(device, non_blocking=True)
             seq_img = seq_img.view(-1, 196, 16*16*3) 
             seq_pos = seq_pos.to(device, non_blocking=True)
+            seq_size = seq_size.to(device, non_blocking=True)
             # images = torch.reshape(images,shape=(-1,3,224, 224))
             labels = labels.to(device, non_blocking=True)
             optimizer.zero_grad()   
@@ -96,6 +97,7 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, num_epoch
                 # images = torch.reshape(images,shape=(-1,3,224, 224))
                 labels = labels.to(device, non_blocking=True)
                 seq_pos = seq_pos.to(device, non_blocking=True)
+                seq_size = seq_size.to(device, non_blocking=True)
                 
                 with torch.cuda.amp.autocast():
                     try:
