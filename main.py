282ac6195d20590f0a88880d220e7e2692bd5b1e
zezzz <zezzz@zezs-mac-mini.lan>
2025-02-11 17:39:45 +0900

    test adaptive forest.

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ def parse_args():
     parser.add_argument('--gpus', type=int, default=8, help='Epochs for iteration')
     parser.add_argument('--nodes', type=int, default=1, help='Epochs for iteration')
     parser.add_argument('--data_dir', type=str, default='/Volumes/data/dataset/imagenet', help='Path to the ImageNet dataset directory')
+    parser.add_argument('--seq_length', type=int, default=196, help='Epochs for iteration')
     parser.add_argument('--num_epochs', type=int, default=3, help='Epochs for iteration')
     parser.add_argument('--batch_size', type=int, default=128, help='Batch size for DataLoader')
     parser.add_argument('--num_workers', type=int, default=8, help='Number of workers for DataLoader')
