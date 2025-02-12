7ad43c07e4fd67d7550a033769fa3a0db9576bbc
zezzz <zezzz@zezs-mac-mini.lan>
2025-02-12 20:12:59 +0900

    test adaptive forest.

--- train/vit_imagenet_af.py
+++ train/vit_imagenet_af.py
@@ -153,7 +153,7 @@ def vit_af_train(args):
     # Create ViT model
     # model = create_vit_model(args.pretrained)
     model = AF_ViT(num_classes=1000, seq_length=args.seq_length)
-    # model = nn.DataParallel(model)
+    model = nn.DataParallel(model)
     model = model.to(device)
     save_path = os.path.join(args.output, args.savefile)
     if args.reload:
