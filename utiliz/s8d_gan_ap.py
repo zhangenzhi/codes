59d3c3f45f41d851b78fb5f79dcbd2916f052801
zhangenzhi <zhangenzhi657@gmail.com>
2025-03-14 14:30:03 +0800

    test s8d gan ap loader.

--- utiliz/s8d_gan_ap.py
+++ utiliz/s8d_gan_ap.py
@@ -34,6 +34,17 @@ if __name__ == "__main__":
     root_dir = "/lustre/orion/mat268/world-shared/RIKEN/simulation_XCT/Noisy0.5_900views_detector800x800_12um/FBP"  # Change this to your directory
     import pdb;pdb.set_trace()
     
+    from PIL import Image
+    img_path = "/lustre/orion/mat268/world-shared/RIKEN/simulation_XCT/Noisy0.5_900views_detector800x800_12um/FBP/VOL_104_ConcA_angular_flack33_sph_p70Vf_dm_p15_pm_p15_r_p15_UC60_reconFBPsimul_Crop150.tiff"
+
+    try:
+        img = Image.open(img_path)
+        img.verify()  # Verify if it's a valid image
+        print("Image format:", img.format)
+    except Exception as e:
+        print("Error:", e)
+
+
     image_files = [f for f in os.listdir(root_dir) if f.endswith('.tiff')]
     idx = 0
     img_path = os.path.join(root_dir, image_files[idx])
